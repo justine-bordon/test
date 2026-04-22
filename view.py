a457653cede89b31e976b3290714d17586175883
justine-bordon <jtbordon@up.edu.ph>
2026-04-22 22:07:40 +0800

    Add files via upload

--- view.py
+++ view.py
@@ -27,35 +27,41 @@ class View:
 		pyxel.init(self.width, self.height, title="Whac-A-Mole (Pyxel Version)")
 		pyxel.mouse(True)
 		self._active_mole_count = 0
+		self._LOCS = {
+		6:[(100,100), (100,50), (50,100), (75,125), (125,125), (150,100)],
+		7:[(100,100), (100,50), (75,75), (75,125), (100,150), (125,125), (125,75)],
+		8:[(100,100), (100,50), (50,75), (50,125), (75,150), (125,150), (150, 125), (150,75)]
+		}
 	def start_game(self, update_handler: UpdateHandler, draw_handler: DrawHandler) -> None:
 		pyxel.run(update_handler.update, draw_handler.draw)
 	def get_clicked_mole(self) -> int | None:
 		if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT):
-			for i in range(self._active_mole_count):
-				col = i % 3
-				row = i // 3
-				cx = 40 + (col * 60)
-				cy = 40 + (row*60)
-				
-				if (pyxel.mouse_x - cx)**2 + (pyxel.mouse_y - cy)**2 <= 400:
-					return i
+			total = self._active_mole_count
+			locs = self._LOCS.get(total)
+			
+			if locs:
+				for i in range(total):
+					cx, cy = layout[i]
+					if (pyxel.mouse_x - cx)**2 + (pyxel.mouse_y - cy)**2 <= 100:
+						return i
 		return None
 				
 	def draw_moles(self, moles_info: Sequence[MoleInfo]) -> None:
 		self._active_mole_count = len(moles_info)
+		total = self._active_mole_count
+		locs = self._LOCS.get(total)
 		
 		for i, mole in enumerate(moles_info):
-			col = i % 3
-			row = i // 3
-			x = 40 + (col*60)
-			y = 40 + (row*60)
+			x, y = layout[i]
+			
+			pyxel.circ(x,y,15,5)
 			
 			if mole.state == MoleState.ACTIVE:
-				pyxel.circ(x,y,20,9)
+				pyxel.circ(x,y,10,9)
 			elif mole.state == MoleState.HIT:
-				pyxel.circ(x,y,20,8)
+				pyxel.circ(x,y,10,8)
 			else:
-				pyxel.circ(x,y,20,5)
+				pyxel.circ (x,y,10,5)
 		
 		
 		
