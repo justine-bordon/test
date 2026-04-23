10af664cb4db7ac824d4e93fd1ca94ba83d57783
justine-bordon <jtbordon@up.edu.ph>
2026-04-23 13:19:42 +0800

    Update model.py

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from typing import Protocol
 from moles import (
     MoleState, MoleInfo, Mole,
     MolePopupPlan, GameOverCondition,
-    SimpleMole, BombMole
+    SimpleMole, BombMole, LuckyMole, RichMole
 )
 
 
@@ -86,5 +86,5 @@ class Model:
         rng = Random()
         simple_popup = SimpleMolePopupPlan()
         simple_game_over = SimpleGameOverCondition()
-        moles = [SimpleMole() for _ in range(7)] + [BombMole()]
+        moles = [SimpleMole() for _ in range(5)] + [BombMole()] + [LuckyMole()] + [RichMole()]
         return cls(moles, simple_popup, simple_game_over, rng)
